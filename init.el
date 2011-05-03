89e084defec036adf8d9aabe4b3c863d6bb9a8d1
Dimitri Fontaine <dim@tapoueh.org>
2011-05-03 20:47:25 +0200

    revert previous patch, as it results in network dependency to start up emacs

--- init.el
+++ init.el
@@ -9,11 +9,14 @@
 ;;
 ;; This file is NOT part of GNU Emacs.
 
-(url-retrieve
- "https://github.com/dimitri/el-get/raw/master/el-get-install.el"
- (lambda (s)
-   (end-of-buffer)
-   (eval-print-last-sexp)))
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+(unless (require 'el-get nil t)
+  (url-retrieve
+   "https://github.com/dimitri/el-get/raw/master/el-get-install.el"
+   (lambda (s)
+     (end-of-buffer)
+     (eval-print-last-sexp))))
 
 (require 'el-get)
 (setq
