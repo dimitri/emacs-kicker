24157efadef96f93d4d6748be86189c7fe306ecf
Dimitri Fontaine <dim@tapoueh.org>
2011-05-03 20:46:31 +0200

    Following-up on recent changes in el-get, el-get-installer will care for the load-path itself.

--- init.el
+++ init.el
@@ -9,14 +9,11 @@
 ;;
 ;; This file is NOT part of GNU Emacs.
 
-(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
-
-(unless (require 'el-get nil t)
-  (url-retrieve
-   "https://github.com/dimitri/el-get/raw/master/el-get-install.el"
-   (lambda (s)
-     (end-of-buffer)
-     (eval-print-last-sexp))))
+(url-retrieve
+ "https://github.com/dimitri/el-get/raw/master/el-get-install.el"
+ (lambda (s)
+   (end-of-buffer)
+   (eval-print-last-sexp)))
 
 (require 'el-get)
 (setq
