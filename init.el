b4bb960f9ef63f448015eb70b0ba7c88e4180d18
Dimitri Fontaine <dim@tapoueh.org>
2014-07-22 11:47:19 +0200

    Switch to current el-get init forms, see #6.
    Lambda forms have been deprecated for a long time already and now force
    warnings at el-get initalization time.

--- init.el
+++ init.el
@@ -27,24 +27,24 @@
 (setq
  el-get-sources
  '((:name buffer-move			; have to add your own keys
-	  :after (lambda ()
+	  :after (progn
 		   (global-set-key (kbd "<C-S-up>")     'buf-move-up)
 		   (global-set-key (kbd "<C-S-down>")   'buf-move-down)
 		   (global-set-key (kbd "<C-S-left>")   'buf-move-left)
 		   (global-set-key (kbd "<C-S-right>")  'buf-move-right)))
 
    (:name smex				; a better (ido like) M-x
-	  :after (lambda ()
+	  :after (progn
 		   (setq smex-save-file "~/.emacs.d/.smex-items")
 		   (global-set-key (kbd "M-x") 'smex)
 		   (global-set-key (kbd "M-X") 'smex-major-mode-commands)))
 
    (:name magit				; git meet emacs, and a binding
-	  :after (lambda ()
+	  :after (progn
 		   (global-set-key (kbd "C-x C-z") 'magit-status)))
 
    (:name goto-last-change		; move pointer back to last change
-	  :after (lambda ()
+	  :after (progn
 		   ;; when using AZERTY keyboard, consider C-x C-_
 		   (global-set-key (kbd "C-x C-/") 'goto-last-change)))))
 
